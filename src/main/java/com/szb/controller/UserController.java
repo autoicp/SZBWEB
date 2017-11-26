aedf0bccb2d205c339dcb25cd33ea5c808aa8afa
qinjianfeng <33476554+qinjianfeng@users.noreply.github.com>
2017-11-26 17:38:59 +0800

    Add files via upload
    支持用户关注时上传收款码

--- src/main/java/com/szb/controller/UserController.java
+++ src/main/java/com/szb/controller/UserController.java
@@ -3,15 +3,22 @@ package com.szb.controller;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
+import java.io.File;
+import java.io.IOException;
 import java.sql.Timestamp;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.ibatis.io.Resources;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;  
 import org.springframework.ui.Model;  
 import org.springframework.web.bind.annotation.RequestMapping;  
   
 import com.szb.pojo.User;  
-import com.szb.service.UserService;  
-  
+import com.szb.service.UserService;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
 @SuppressWarnings("restriction")
 @Controller  
 @RequestMapping("/user")  
@@ -35,7 +42,7 @@ public class UserController {
 	 * @return
 	 */
 	@RequestMapping("/usercenterdo")
-	public String toIndex2(HttpServletRequest request){
+	public String toIndex2(@RequestParam("qrCode") MultipartFile qrCode, HttpServletRequest request){
 		String userName = request.getParameter("userName");
 		String telephone = request.getParameter("telephone");
 		String shopName = request.getParameter("shopName");
@@ -51,6 +58,16 @@ public class UserController {
 		user.setShopAddress(shopAddress);
 		user.setRegistTime(registTime);
 		this.userService.insert(user);
+		user = this.userService.selectBySelective(user);
+		//TODO 收钱码地址暂定为D:\Program Files\tomcat\apache-tomcat-8.0.20\bin\6dc5e11f-d28a-11e7-8d04-e8039a352be6.jpg
+		try {
+			FileUtils.copyInputStreamToFile(qrCode.getInputStream(),new File(new File("").getCanonicalPath()+"\\"+user.getId()+"."+qrCode.getOriginalFilename().split("\\.")[1]));
+		} catch (IOException e) {
+			e.printStackTrace();
+			request.setAttribute("message","上传失败");
+			return "failed";
+		}
+
 		request.setAttribute("message" ,"insert success");
 		return "success";
 	}
