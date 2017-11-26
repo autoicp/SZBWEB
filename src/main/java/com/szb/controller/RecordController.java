891c98d33eaa475dcf397901a588725d4f72f5e0
qinjianfeng <33476554+qinjianfeng@users.noreply.github.com>
2017-11-26 09:21:34 +0800

    Add files via upload
    添加一些页面跳转，比如账单创建成功后跳转到账单创建成功页

--- src/main/java/com/szb/controller/RecordController.java
+++ src/main/java/com/szb/controller/RecordController.java
@@ -25,13 +25,14 @@ public class RecordController {
     @Resource
     private RecordService recordService;
 
+
     /**
      *提供记账入口
      * @param request
      * @return
      */
     @RequestMapping("/account")
-    public String toIndex1(HttpServletRequest request){
+    public String account(HttpServletRequest request){
         return "account";
     }
 
@@ -41,7 +42,7 @@ public class RecordController {
      * @return
      */
     @RequestMapping("/accountdo")
-    public String toIndex2(HttpServletRequest request){
+    public String accountdo(HttpServletRequest request){
 
         //获取记账参数
         String productname = request.getParameter("productname");
@@ -66,6 +67,105 @@ public class RecordController {
         Logger logger = Logger.getLogger(this.getClass());
         logger.debug("记账一次");
 
-        return "success";
+        return "accountsuccess";
+    }
+
+    /**
+     *提供账单确认入口
+     * @param request
+     * @return
+     */
+    @RequestMapping("/accountconfirm")
+    public String accountconfirm(HttpServletRequest request){
+        return "accountconfirm";
+    }
+
+    /**
+     *账单确认
+     * @param request
+     * @return
+     */
+    @RequestMapping("/accountconfirmdo")
+    public String accountconfirmdo(HttpServletRequest request){
+
+        //TODO 账单确认（还需添加if、else实现账单取消）
+        return "accountconfsuc";
+    }
+    /**
+     *账单催收
+     * @param request
+     * @return
+     */
+    @RequestMapping("/collection")
+    public String accountconfsuc(HttpServletRequest request){
+
+        //TODO 账单催收
+        return "collectionsuc";
+    }
+
+    /**
+     *账单列表
+     * @param request
+     */
+    @RequestMapping("/debtlist")
+    public String debtlist(HttpServletRequest request){
+
+        //TODO 账单列表（把账单信息存到request里）,注意有代收账款和已收账款，看是否需要两个方法
+        return "debtlist";
+    }
+
+    /**
+     *账单详情
+     * @param request
+     */
+    @RequestMapping("/debtdetail")
+    public String debtdetail(HttpServletRequest request){
+
+        //TODO 账单详情（把账单信息存到request里）
+        return "debtdetail";
+    }
+
+    /**
+     *已支付账单详情
+     * @param request
+     */
+    @RequestMapping("/haspaid")
+    public String haspaid(HttpServletRequest request){
+
+        //TODO 已支付账单详情（把账单信息存到request里）
+        return "haspaid";
+    }
+
+    /**
+     *已收账单详情
+     * @param request
+     */
+    @RequestMapping("/paiddetail")
+    public String paiddetail(HttpServletRequest request){
+
+        //TODO 已收账单详情（把账单信息存到request里）
+        return "paiddetail";
+    }
+
+    /**
+     *支付入口
+     * @param request
+     */
+    @RequestMapping("/pay")
+    public String pay(HttpServletRequest request){
+
+        //TODO 把账单信息存到request里，收款码等的展示
+        return "pay";
+    }
+
+    /**
+     *待支付入口，提供支付信息
+     * @param request
+     */
+    @RequestMapping("/topay")
+    public String topay(HttpServletRequest request){
+
+        //TODO 把账单信息存到request里
+        return "topay";
     }
 }
