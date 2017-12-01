5e6069c391510d04bf881e8d8919b1a31b0e4d31
qinjianfeng <33476554+qinjianfeng@users.noreply.github.com>
2017-12-01 22:56:16 +0800

    Add files via upload

---
+++ src/main/resources/record.sql
@@ -0,0 +1,30 @@
+﻿# Host: localhost  (Version: 5.5.15)
+# Date: 2017-12-01 22:55:20
+# Generator: MySQL-Front 5.3  (Build 4.38)
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "record"
+#
+
+CREATE TABLE `record` (
+  `Id` varchar(40) NOT NULL DEFAULT '' COMMENT '账单ID',
+  `floatNum` varchar(40) DEFAULT NULL COMMENT '账单流水号',
+  `hostChatId` varchar(30) DEFAULT NULL COMMENT '商家ID',
+  `productName` varchar(40) DEFAULT NULL COMMENT '商品名称',
+  `productCounts` varchar(6) DEFAULT NULL COMMENT '商品数量',
+  `productAmount` varchar(10) DEFAULT NULL COMMENT '商品总金额',
+  `occurTime` timestamp NULL DEFAULT NULL COMMENT '账目发生时间',
+  `duePayDate` date DEFAULT NULL COMMENT '应还款日',
+  `payoutTime` timestamp NULL DEFAULT NULL COMMENT '还款时间',
+  `custChatId` varchar(30) DEFAULT NULL COMMENT '客户chatID',
+  `paymentStatus` varchar(1) DEFAULT '0' COMMENT '还款状态(0:未还；1:已还)',
+  `checkStatus` varchar(2) DEFAULT '0' COMMENT '客户确认状态(0:未确认；1:已确认)',
+  `checkTime` timestamp NULL DEFAULT NULL COMMENT '订单确认时间',
+  `hostUuid` varchar(255) DEFAULT NULL COMMENT '商家UUID',
+  `custUuid` varchar(255) DEFAULT NULL COMMENT '客户UUID',
+  PRIMARY KEY (`Id`),
+  KEY `customer_chat_id` (`custChatId`) COMMENT '按买家查找',
+  KEY `host_chat_id` (`hostChatId`) COMMENT '按商家查找'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='账单';
---
+++ src/main/resources/user.sql
@@ -0,0 +1,27 @@
+﻿# Host: localhost  (Version: 5.5.15)
+# Date: 2017-12-01 22:55:04
+# Generator: MySQL-Front 5.3  (Build 4.38)
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "user"
+#
+
+CREATE TABLE `user` (
+  `Id` varchar(40) NOT NULL DEFAULT '',
+  `userName` varchar(30) DEFAULT NULL COMMENT '姓名',
+  `telephone` varchar(11) DEFAULT NULL COMMENT '手机号',
+  `shopAddress` varchar(255) DEFAULT NULL COMMENT '公司或店铺名称',
+  `homeAddress` varchar(255) DEFAULT NULL COMMENT '家庭住址',
+  `userType` varchar(2) DEFAULT NULL COMMENT '用户类型（店铺/商家）',
+  `chatId` varchar(255) DEFAULT NULL COMMENT '微信号',
+  `shopName` varchar(255) DEFAULT NULL COMMENT '店铺或公司名称',
+  `qrCodeAddress` varchar(255) DEFAULT NULL COMMENT '二维码地址（存储路径）',
+  `headPortraitAddress` varchar(255) DEFAULT NULL COMMENT '头像地址',
+  `nickName` varchar(255) DEFAULT NULL COMMENT '昵称',
+  `registTime` timestamp NULL DEFAULT NULL COMMENT '注册时间',
+  `changeTime` timestamp NULL DEFAULT NULL COMMENT '修改时间',
+  PRIMARY KEY (`Id`),
+  KEY `chat_id` (`chatId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户表';
